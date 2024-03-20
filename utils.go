46f3d921a78e6edf5e48b8be2e1d6a99ed91190c
João Ribeiro <joaosoft@gmail.com>
2024-03-20 14:54:48 +0100

    upgrade go 1.22.1

--- utils.go
+++ utils.go
@@ -42,7 +42,7 @@ func ReadFile(file string, obj interface{}) ([]byte, error) {
 	var err error
 
 	if !Exists(file) {
-		return nil, errors.New(errors.ErrorLevel, 0, "file don't exist")
+		return nil, errors.New(errors.LevelError, 0, "file don't exist")
 	}
 
 	f, err := os.Open(file)
@@ -68,7 +68,7 @@ func ReadFileLines(file string) ([]string, error) {
 	lines := make([]string, 0)
 
 	if !Exists(file) {
-		return nil, errors.New(errors.ErrorLevel, 0, "file don't exist")
+		return nil, errors.New(errors.LevelError, 0, "file don't exist")
 	}
 
 	f, err := os.Open(file)
@@ -91,7 +91,7 @@ func ReadFileLines(file string) ([]string, error) {
 
 func WriteFile(file string, obj interface{}) error {
 	if !Exists(file) {
-		return errors.New(errors.ErrorLevel, 0, "file don't exist")
+		return errors.New(errors.LevelError, 0, "file don't exist")
 	}
 
 	jsonBytes, _ := json.MarshalIndent(obj, "", "    ")
